cc03bedbdc30dd9a6809b59fb0c46edadb8b3e88
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-11-30 22:48:19 -0800

    asset cache

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -18,10 +18,9 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transanction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(content);
+  const request = store.put({ id: 1, value:content});
   const result = await request;
   console.log('result.value', result);
-  return result?.value;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -35,4 +34,5 @@ const result = await request;
 console.log('result.value', result);
 return result;
 };
+
 initdb();
